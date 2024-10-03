22edaae2641b48a9c5def1f36ac8248f508fc304
Raul Sousa <raulsousadesigner@gmail.com>
2024-10-03 09:11:05 -0300

    Add: Main menu + Modos de Jogo (Versão Jogável

--- main.py
+++ main.py
@@ -11,28 +11,27 @@ class Jogo:
         self.running = True
         self.bg = pygame.image.load(bgCaminho)
         self.mainMenu = pygame.image.load(mainMenuCaminho)
-        self.menu = Menu(self.tela, mainMenuCaminho)
+        self.menu = Menu(self.tela, self.mainMenu)
+        self.estado = 'menu'
 
         #Sprites
         self.sprites = pygame.sprite.Group()
         self.barrinhaSprites = pygame.sprite.Group()
-        self.player1 = Player((self.sprites, self.barrinhaSprites),1)
-        self.bola = Bola(self.sprites, self.barrinhaSprites,self.gol)
-        #self.player2 = Player((self.sprites, self.barrinhaSprites),2) Jogável
-        self.player2 = IA((self.sprites, self.barrinhaSprites),2,self.bola) #IA
+        self.bola = None
+        self.player1 = None
+        self.player2 = None
 
         #Placar
-        self.placar = {'player1':0,'player2':0}
-        self.font = pygame.font.Font(None,160)
-
+        self.placar = {'player1': 0, 'player2': 0}
+        self.font = pygame.font.Font(None, 160)
 
     def mostrarPlacar(self):
-        player1_surf = self.font.render(str(self.placar['player1']),True,cores['placar'])
-        player1_rect = player1_surf.get_rect(center=(telaLargura//2+250, telaAltura//2))
+        player1_surf = self.font.render(str(self.placar['player1']), True, cores['placar'])
+        player1_rect = player1_surf.get_rect(center=(telaLargura//2 + 250, telaAltura//2))
         self.tela.blit(player1_surf, player1_rect)
 
-        player2_surf = self.font.render(str(self.placar['player2']),True,cores['placar'])
-        player2_rect = player2_surf.get_rect(center=(telaLargura//2-250, telaAltura//2))
+        player2_surf = self.font.render(str(self.placar['player2']), True, cores['placar'])
+        player2_rect = player2_surf.get_rect(center=(telaLargura//2 - 250, telaAltura//2))
         self.tela.blit(player2_surf, player2_rect)
 
     def gol(self, lado):
@@ -41,27 +40,48 @@ class Jogo:
         elif lado == 'player2':
             self.placar['player2'] += 1
 
-    def run(self):
+    def iniciarJogo(self, modo):
+        # Reinicia sprites e configura oponentes
+        self.sprites.empty()
+        self.barrinhaSprites.empty()
+
+        self.player1 = Player((self.sprites, self.barrinhaSprites), 1)
+        self.bola = Bola(self.sprites, self.barrinhaSprites, self.gol)
         
+        if modo == 'humanos':
+            self.player2 = Player((self.sprites, self.barrinhaSprites), 2)  # Player 2 jogável
+        elif modo == 'ia':
+            self.player2 = IA((self.sprites, self.barrinhaSprites), 2, self.bola)  # Player 2 IA
+
+    def run(self):
         while self.running:
-            
             dt = self.fps.tick(60) / 1000
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                if event.type == pygame.MOUSEBUTTONDOWN and self.estado == 'menu':
+                    area_humanos, area_ia = self.menu.desenharMenu(self.tela, self.mainMenu)
+                    if self.menu.click_botao(area_humanos, mouse_x, mouse_y):
+                        
+                        # Jogar contra outro humano
+                        self.iniciarJogo('humanos')
+                        self.estado = 'jogo'
 
-            # Atualizar sprites
-            
-            
-            self.sprites.update(dt)
-            
+                    elif self.menu.click_botao(area_ia, mouse_x, mouse_y):
+                        # Jogar contra IA
+                        self.iniciarJogo('ia')
+                        self.estado = 'jogo'
 
-            self.menu.desenharMenu(self.tela,self.mainMenu)
-            while False:
-                self.tela.blit(self.bg,(0,0))
+            # Lógica do menu ou do jogo
+            if self.estado == 'menu':
+                self.menu.desenharMenu(self.tela, self.mainMenu)
+            elif self.estado == 'jogo':
+                self.tela.blit(self.bg, (0, 0))
                 self.mostrarPlacar()
+                self.sprites.update(dt)
                 self.sprites.draw(self.tela)
-            
 
             pygame.display.update()
 
--- menus.py
+++ menus.py
@@ -7,11 +7,10 @@ class Menu:
 
     def desenharMenu(self,tela,mainMenu):
         self.tela.blit(mainMenu,(0,0))
-        pygame.draw.rect(tela, cores['barrinha'], pygame.Rect(800, 200, 200, 100))  # Exemplo com largura 200 e altura 100
 
-        area_iniciar = (480 + 37, 120 + 220, 540, 130)
-        area_sair = (480 + 37, 270 + 220, 540, 130)
-        return area_iniciar, area_sair
+        area_humanos = (460, 346, 337, 100)
+        area_ia = (460, 481, 337, 100)
+        return area_humanos, area_ia
 
     def click_botao(self, area, mouse_x, mouse_y):
         return area[0] <= mouse_x <= area[0] + area[2] and area[1] <= mouse_y <= area[1] + area[3]
